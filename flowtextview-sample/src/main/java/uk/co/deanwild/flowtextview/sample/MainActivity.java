fcf5c0ed402d21584e66e73b93df558efb6577ba
Sebastian Niemeyer <sebastian@lunarisapps.com>
2024-08-20 17:19:29 +0200

    apply code formatter

--- flowtextview-sample/src/main/java/uk/co/deanwild/flowtextview/sample/MainActivity.java
+++ flowtextview-sample/src/main/java/uk/co/deanwild/flowtextview/sample/MainActivity.java
@@ -14,66 +14,63 @@ import uk.co.deanwild.flowtextview.listeners.OnLinkClickListener;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-	
-	private static final float defaultFontSize = 20.0f;
-	
-	private FlowTextView flowTextView;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		flowTextView = (FlowTextView) findViewById(R.id.ftv);
-		String content = getString(R.string.lorem);
-		Spanned html = Html.fromHtml(content);
-		flowTextView.setText(html);
-		
-		
-		// handle link behaviour
-		flowTextView.setOnLinkClickListener(new OnLinkClickListener() {
-			@Override
-			public void onLinkClick(View view, String url) {
-				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-				startActivity(browserIntent);
-			}
-		});
-		
-		Button btnIncreasefontSize = (Button) findViewById(R.id.btn_increase_font_size);
-		btnIncreasefontSize.setOnClickListener(this);
-		Button btnDecreasefontSize = (Button) findViewById(R.id.btn_decrease_font_size);
-		btnDecreasefontSize.setOnClickListener(this);
-		Button btnReset = (Button) findViewById(R.id.btn_reset);
-		btnReset.setOnClickListener(this);
-	}
-	
-	@Override
-	public void onClick(View v) {
-		switch(v.getId()) {
-			case R.id.btn_increase_font_size:
-				increaseFontSize();
-				break;
-			case R.id.btn_decrease_font_size:
-				decreaseFontSize();
-				break;
-			case R.id.btn_reset:
-				reset();
-				break;
-			default:
-				break;
-		}
-	}
-	
-	private void increaseFontSize() {
-		float currentFontSize = flowTextView.getTextsize();
-		flowTextView.setTextSize(currentFontSize + 1);
-	}
-	
-	private void decreaseFontSize() {
-		float currentFontSize = flowTextView.getTextsize();
-		flowTextView.setTextSize(currentFontSize - 1);
-	}
-	
-	private void reset() {
-		flowTextView.setTextSize(defaultFontSize);
-	}
+    
+    private static final float defaultFontSize = 20.0f;
+    
+    private FlowTextView flowTextView;
+    
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btn_increase_font_size:
+                increaseFontSize();
+                break;
+            case R.id.btn_decrease_font_size:
+                decreaseFontSize();
+                break;
+            case R.id.btn_reset:
+                reset();
+                break;
+            default:
+                break;
+        }
+    }
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        flowTextView = findViewById(R.id.ftv);
+        String content = getString(R.string.lorem);
+        Spanned html = Html.fromHtml(content);
+        flowTextView.setText(html);
+        
+        
+        // handle link behaviour
+        flowTextView.setOnLinkClickListener((view, url) -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(browserIntent);
+        });
+        
+        Button btnIncreasefontSize = findViewById(R.id.btn_increase_font_size);
+        btnIncreasefontSize.setOnClickListener(this);
+        Button btnDecreasefontSize = findViewById(R.id.btn_decrease_font_size);
+        btnDecreasefontSize.setOnClickListener(this);
+        Button btnReset = findViewById(R.id.btn_reset);
+        btnReset.setOnClickListener(this);
+    }
+    
+    private void increaseFontSize() {
+        float currentFontSize = flowTextView.getTextsize();
+        flowTextView.setTextSize(currentFontSize + 1);
+    }
+    
+    private void decreaseFontSize() {
+        float currentFontSize = flowTextView.getTextsize();
+        flowTextView.setTextSize(currentFontSize - 1);
+    }
+    
+    private void reset() {
+        flowTextView.setTextSize(defaultFontSize);
+    }
 }
